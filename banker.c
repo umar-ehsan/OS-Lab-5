3fed45d8d31db1923681f7482deab810a8f8d1d6
umar-ehsan <muhammad.ehsan@uoit.net>
2019-03-19 15:17:39 -0400

    Fixed threads

--- banker.c
+++ banker.c
@@ -10,12 +10,13 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 #include "banker.h"
 
 // Put any other macros or constants here using #define
 // May be any values >= 0
 //#define NUM_CUSTOMERS 5
-#define NUM_CUSTOMERS 3
+#define NUM_CUSTOMERS 2
 #define NUM_RESOURCES 3
 
 
@@ -38,7 +39,7 @@ int need[NUM_CUSTOMERS][NUM_RESOURCES] = {0};
 
 int main(int argc, char *argv[]){
     // ==================== YOUR CODE HERE ==================== //
-    ///*
+    
     // Read in arguments from CLI, NUM_RESOURCES is the number of arguments   
     if (argc != 4){
         printf("\nUsage: ./banker <argument 1> <argument 2> <argument 3>\nWhere <argument n> is an integer representing the resource amount.\n\n");
@@ -72,37 +73,17 @@ int main(int argc, char *argv[]){
         printf("\nError initializing mutex locks. Terminating...\n\n");
         return EXIT_FAILURE;
     }
-    //*/
     getchar();
     // Run the threads and continually loop
     // The threads will request and then release random numbers of resources
     while (1){
-        int n_arg = 0;
-        printf("\nRequesting resources: \n\n");
+        printf("\nRequesting resources: \n");
         for (int i = 0; i < NUM_CUSTOMERS; i++){
-            n_arg = i;
-            printf("Process #%d:\n", n_arg);
-            pthread_create(&customers[i], NULL, res_request, (void*)&n_arg);
-            //if (pthread_create(&customers[i], NULL, res_request, (void*)&n_arg) != 0){
-            //    printf("\nError creating thread, terminating...\n\n");
-            //    return EXIT_FAILURE;
-            //}
-        }
-        for (int i = 0; i < NUM_CUSTOMERS; i++){
-            if (pthread_join(customers[i], NULL) != 0){
-                printf("\nError joining thread, terminating...\n\n");
-                return EXIT_FAILURE;
-            }
-        }
-        printf("Press any key to continue...\n");
-        getchar();
-        printf("\nReleasing resources: \n\n");
-        for (int i = 0; i < NUM_CUSTOMERS; i++){
-            n_arg = i;
-            if (pthread_create(&customers[i], NULL, res_release, (void*)&n_arg) != 0){
+            if (pthread_create(&customers[i], NULL, res_request, (void*)&i) != 0){
                 printf("\nError creating thread, terminating...\n\n");
                 return EXIT_FAILURE;
             }
+            sleep(1);
         }
         for (int i = 0; i < NUM_CUSTOMERS; i++){
             if (pthread_join(customers[i], NULL) != 0){
@@ -110,8 +91,22 @@ int main(int argc, char *argv[]){
                 return EXIT_FAILURE;
             }
         }
-        printf("Press any key to continue...\n");
-        getchar();
+        sleep(3);
+        printf("\nReleasing resources:: \n");
+        for (int i = 0; i < NUM_CUSTOMERS; i++){
+            if (pthread_create(&customers[i], NULL, res_release, (void*)&i) != 0){
+                printf("\nError creating thread, terminating...\n\n");
+                return EXIT_FAILURE;
+            }
+            sleep(1);
+        }
+        for (int i = 0; i < NUM_CUSTOMERS; i++){
+            if (pthread_join(customers[i], NULL) != 0){
+                printf("\nError joining thread, terminating...\n\n");
+                return EXIT_FAILURE;
+            }
+        }
+        sleep(3);
     }
 
     return EXIT_SUCCESS;
@@ -119,15 +114,13 @@ int main(int argc, char *argv[]){
 
 // Requests a resource
 void *res_request(void *arg){
-    printf("hmm: %d\n", *((int*)arg));
     pthread_mutex_lock(&request_lock);
     int n_process = *((int*)arg);
     int request[NUM_RESOURCES];
-    printf("Process %d requesting resources\n\n", n_process);
     for (int i = 0; i < NUM_RESOURCES; i++){
         request[i] = rand() % maximum[n_process][i];
     }
-    printf("Process #%d requesting the resources: [", n_process);
+    printf("\nProcess #%d requesting the resources: [", n_process);
     for (int i = 0; i < NUM_RESOURCES; i++){
         printf("%d,", request[i]);
     }
@@ -138,7 +131,6 @@ void *res_request(void *arg){
     else {
         printf("Unable to allocate the specified resources.\n");
     }
-    printf("\nProcess %d exiting resource request routine\n\n", n_process);
     pthread_mutex_unlock(&request_lock);
     return NULL;
 }
@@ -147,12 +139,11 @@ void *res_request(void *arg){
 void *res_release(void *arg){
     pthread_mutex_lock(&release_lock);
     int n_process = *((int*)arg);
-    printf("Process %d releasing resources\n\n", n_process);
     int request[NUM_RESOURCES];
     for (int i = 0; i < NUM_RESOURCES; i++){
         request[i] = rand() % maximum[n_process][i];
     }
-    printf("Process #%d releasing the resources: [", n_process);
+    printf("\nProcess #%d releasing the resources: [", n_process);
     for (int i = 0; i < NUM_RESOURCES; i++){
         printf("%d,", request[i]);
     }
@@ -163,7 +154,6 @@ void *res_release(void *arg){
     else {
         printf("Unable to release the specified resources.\n");
     }
-    printf("\nProcess %d exiting resource release routine\n\n", n_process);
     pthread_mutex_unlock(&release_lock);
     return NULL;
 }
